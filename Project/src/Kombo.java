a1ff291e3739b0010bd776053aafed5c7a26afb4
livej0urnal <live_j0urnal@mail.ua>
2019-09-14 22:05:27 +0300

    Add all my files

---
+++ Project/src/Kombo.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class Kombo extends Application {
+
+    public static void main(String[] args) {
+        System.out.println("JavaFX");
+        launch(args);
+    }
+
+    public void init()
+    {
+        System.out.println("init");
+    }
+
+    @Override
+    public void start(Stage myStage) {
+        System.out.println("V tele");
+        myStage.setTitle("Title");
+        FlowPane rootNode = new FlowPane();
+        Scene myScene = new Scene(rootNode , 1000 , 700);
+        myStage.setScene(myScene);
+        myStage.show();
+    }
+
+    public void stop()
+    {
+        System.out.println("stop");
+    }
+}
